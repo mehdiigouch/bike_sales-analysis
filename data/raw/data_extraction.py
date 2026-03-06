83fd544f7f01168cf7d672fae793f6c9789fee9f
el mehdi  iggouch <elmehdi.iggouch@gmail.com>
2026-03-06 22:04:36 +0000

    fixing some bugs

---
+++ data/raw/data_extraction.py
@@ -0,0 +1,93 @@
+
+#  uv run python data/raw/data_extraction.py  IF YOU WANNA RUN THE CODE USE THIS COMMAND 
+import pandas as pd
+from utils.logs import section,logger
+from pathlib import Path
+import time
+
+
+
+PATH_ROOT  = Path.cwd().parent
+print(PATH_ROOT)
+DATASET_ROOT = PATH_ROOT /"Sales.csv"
+
+
+
+
+# ════════════════════════════════════════════════════════════
+# STEP 1 — VALIDATE FILE EXISTS
+# ════════════════════════════════════════════════════════════
+
+def validate_file(file_path: str) -> Path:
+    section("STEP 1 — FILE VALIDATION")
+    path = Path(file_path).resolve()
+
+    # Check existence
+    if not path.exists():
+        logger.error(f"File not found: {path}")
+        raise FileNotFoundError(f" File not found: {path}")
+    logger.info(f" File found      : {path.name}")
+
+    # Check extension
+    if path.suffix.lower() != ".csv":
+        raise ValueError(f" Expected a .csv file, got: '{path.suffix}'")
+    logger.info(f" File extension  : {path.suffix}")
+
+
+    # Check file size
+    size_kb = path.stat().st_size / 1024
+    size_mb = size_kb / 1024
+    logger.info(f" File size       : {size_mb:.2f} MB ({size_kb:,.1f} KB)")
+    if path.stat().st_size == 0:
+        raise ValueError(" File is empty (0 bytes).")
+
+    logger.info(f" Full path       : {path}")
+
+    return path
+
+
+
+# ════════════════════════════════════════════════════════════
+# STEP 2 — LOAD RAW DATA
+# ════════════════════════════════════════════════════════════
+
+def load_raw_data(path: Path) -> pd.DataFrame:
+
+    section("STEP 2 — LOADING DATA")
+    logger.info("Reading CSV file ...")
+
+    start = time.time()
+
+    try:
+        df = pd.read_csv(path)
+    except pd.errors.EmptyDataError:
+        logger.error("The file is empty or has no columns.")
+        raise
+    except pd.errors.ParserError as e:
+        logger.error(f"CSV parsing error: {e}")
+        raise
+    elapsed = time.time() - start
+
+    logger.info(f"Loaded in       : {elapsed:.2f} seconds")
+    logger.info(f" Rows            : {df.shape[0]:,}")
+    logger.info(f" Columns         : {df.shape[1]}")
+    logger.info(f" Memory usage    : {df.memory_usage(deep=True).sum() / 1024**2:.2f} MB")
+    logger.info(f" Column names    : {df.columns.tolist()}")
+
+    return df
+
+
+
+
+
+
+
+
+
+
+
+df = load_raw_data(validate_file(DATASET_ROOT))      
+
+
+
+
